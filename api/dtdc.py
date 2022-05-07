25dbf587e5ec2b71de92943c72ef419eab00f641
cyberboysumanjay <cyberboysumanjay@gmail.com>
2022-05-07 11:14:20 +0530

    Added DTDC and Bluedart Support

---
+++ api/dtdc.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def get_data(tracking_id):
+    raw_data = {}
+    url = f'http://track.dtdc.com/ctbs-tracking/customerInterface.tr?submitName=getLoadMovementDetails&cnNo={tracking_id}'
+    resp = requests.get(url).json()
+    if resp[0]['activityType'] == "Invalid Data":
+        raw_data['status'] = False
+        return raw_data
+    else:
+        raw_data["status"] = True
+        raw_data['data'] = []
+        for r in resp:
+            data = {}
+            data['tracking_id'] = tracking_id
+            data['activity'] = r['activityType']
+            data['date'] = r['dateWithNoSuffix']
+            data['time'] = r['time']
+            data['status'] = r['deliveryStatus']
+            data['origin'] = r['origin']
+            data['destination'] = r['dest']
+            raw_data['data'].append(data)
+        return raw_data
+    return None
+
+
+# print(get_data("Z74562053"))
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from api import gati, ekart, dtdc
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return {"message": "API is UP!"}
+
+
+@app.get("/active")
+def active():
+    return JSONResponse({"gati": True, "ekart": True})
+
+
+@app.get("/gati")
+@app.get("/gati/{dktno}")
+def _gati(dktno: str):
+    return JSONResponse(gati.get_data(dktno))
+
+
+@app.get("/ekart/{trackingId}")
+def _ekart(trackingId: str):
+    return JSONResponse(ekart.get_data(trackingId))
+
+
+@app.get("/dtdc/{trackingId}")
+def _dtdc(trackingId: str):
+    return JSONResponse(dtdc.get_data(trackingId))
