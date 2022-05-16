5badedb74e2f77b73a69b269edd93aa483ec13c2
Sumanjay <cyberboysumanjay@gmail.com>
2022-05-16 08:42:24 +0530

    Updated Ekart Code

--- api/ekart.py
+++ api/ekart.py
@@ -3,24 +3,21 @@ from requests.structures import CaseInsensitiveDict
 
 
 def get_data(tracking_id):
-    url = "https://www.ekartlogistics.com/ws/getTrackingDetails"
-
+    url = "https://ekartlogistics.com/ws/getTrackingDetails"
     headers = CaseInsensitiveDict()
-    headers["Connection"] = "keep-alive"
     headers["Accept"] = "application/json, text/plain, */*"
-    headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    headers["Accept-Language"] = "en-US,en;q=0.9"
+    headers["Connection"] = "keep-alive"
     headers["Content-Type"] = "application/json"
-    headers["Sec-GPC"] = "1"
-    headers["Origin"] = "https://www.ekartlogistics.com"
-    headers["Sec-Fetch-Site"] = "same-origin"
-    headers["Sec-Fetch-Mode"] = "cors"
+    headers["Cookie"] = "empid=; ab=testprodcluster; epoctime=1652180704.216"
+    headers["Origin"] = "https://ekartlogistics.com"
+    headers["Referer"] = f"https://ekartlogistics.com/shipmenttrack/{tracking_id}"
     headers["Sec-Fetch-Dest"] = "empty"
-    headers["Referer"] = f"https://www.ekartlogistics.com/shipmenttrack/{tracking_id}"
-    headers["Accept-Language"] = "en-GB,en-US;q=0.9,en;q=0.8"
-    data = '{"trackingId":"' + str(tracking_id) + '"}'
-    resp = requests.post(url, headers=headers, data=data)
-    print("Gati Request Returned:", resp.status_code)
-    return resp.json()
+    headers["Sec-Fetch-Mode"] = "cors"
+    headers["Sec-Fetch-Site"] = "same-origin"
+    headers["Sec-GPC"] = "1"
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
+    data = '{"trackingId":"'+ tracking_id+'"}'
+    resp = requests.post(url, headers=headers, data=data).json()
+    return resp
 
-
-# print(get_data("FMPP0845214270"))
